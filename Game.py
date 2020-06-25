9499c8e78a2aa85967c8bc79b8684af024fab40c
Funghu-fps <66675745+Funghu-fps@users.noreply.github.com>
2020-06-25 15:58:07 +0300

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,8 +1,18 @@
 import pygame
-
+from menu import *
 (width, height) = (700, 700)
 running = True
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("GAME")
 
+
+font1 = pygame.font.SysFont("Arial",25)
+
+
+
+activemenu = []
 colors = {
               "white" : (255, 255, 255),
               "silver" : (255, 255, 255),
@@ -27,99 +37,30 @@ colors = {
               "brown" : (117, 74, 49),
               "pink" : (255, 140, 180)
              }
-# msg dan sonra back ground değerine tuple gelmeli örn:(255,255,255) veya colors dan değer
-class single:
-    def __init__(self,screen, pos, font, color, msg, background = False, backcolor = (0,0,0), size = (30,30), width = 0):
-        self.screen = screen
-        self.pos = pos
-        self.font = font
-        self.color = color
-        self.msg = msg
-        self.background = background
-        self.backcolor = backcolor
-        self.size = size
-        self.width = width
 
-        self.text = "Empty"
-        self.text_x = 0
-        self.text_y = 0
-        if self.background:
-            self.text = self.font.render(self.msg, True, self.color)
-            self.text_x = self.size[0]/2 - self.text.get_width()/2 + self.pos[0]
-            self.text_y = self.size[1]/2 - self.text.get_height()/2 + self.pos[1]
-        else:
-            self.text = self.font.render(self.msg, True, self.color)
-            self.text_x = self.size[0]/2 - self.text.get_width()/2 + self.pos[0]
-            self.text_y = self.size[1]/2 - self.text.get_height()/2 + self.pos[1]
-    def render(self):
-        if self.background:
-            pygame.draw.rect(self.screen,self.backcolor,(self.pos,self.size),self.width)
-            self.screen.blit(self.text,(self.text_x,self.text_y))
-        else:
-            self.screen.blit(self.text,(self.text_x,self.text_y))
-
-
-
-#args ın "0" ıncı indeksi tuple olduğunda onu back ground olarak kullanacak rowNline ve gap 2 li tuple olacak
-class menu:
-    def __init__(self, screen, pos, font ,color ,collumNline , gap, *msg, background = False, backcolor = (0,0,0), size = (30,30), width = 0, backgroundcolor = (255,0,0)):
-        self.menu_elements = []
-        self.menu_rects = []
-        self.gapx ,  self.gapy = gap
-        self.posx ,  self.posy = pos
-        self.sizex ,  self.sizey = size
-        self.collum ,  self.line= collumNline
-        self.a=0
-
-        self.screen = screen
-        self.pos = pos
-        self.font = font
-        self.color = color
-        self.msg = msg
-        self.background = background
-        self.backcolor = backcolor
-        self.size = size
-        self.width = width
-        self.backgroundcolor = backgroundcolor
+menus={
+            "mainmenu" : menu(screen, (0,0), font1, colors['white'], (2,3), (10,10), "kebab yes","Test","no","YEmek","elma","birşeyler", background = True, backcolor = colors['olive'], size = (100,50), width = 0, backgroundcolor = colors["magenta"]),
+            "Test" : menu(screen, (0,0), font1, colors['white'], (2,3), (10,10), "elma yes","elma yes","elma no","elma","elma","elma", background = True, backcolor = colors['olive'], size = (100,50), width = 0, backgroundcolor = colors["maroon"])
+    }
         
-        for x in range( self.line):
-            for i in range( self.collum):
-                menu_element=single( self.screen, ( self.posx, self.posy),  self.font,  self.color,  self.msg[i+ self.a],  self.background,  self.backcolor,  self.size,  self.width)
-                self.menu_elements.append(menu_element)
-                self.menu_rects.append(pygame.Rect(( self.posx, self.posy), self.size))
-                self.posx =  self.posx +  self.gapx +  self.sizex
-            self.a+=2
-            self.posx =  self.pos[0]
-            self.posy =  self.posy +  self.sizey +  self.gapy
+       
 
-
+print(type(menus["Test"]))
             
 
 
 
-pygame.init()
-pygame.font.init()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("GAME")
-
-
-font1 = pygame.font.SysFont("Arial",25)
-
-
-
-activemenu = []
-
 
 def main():
-    global running, screen, colors, activemenu,activemenu_rects         
+    global running, screen, colors, activemenu , menus, showmenu        
     screen.fill((255,0,0))
     clock = pygame.time.Clock()
-    showmenu = 1
+    showmenu = True
 
     single1=single(screen,(200,200), font1, (255,255,255),"Kebab OwO",size = (70,90))
 
-    mainmenu = menu(screen, (0,0), font1, colors['white'], (2,3), (10,10), "kebab yes","yes","no","YEmek","elma","birşeyler", background = True, backcolor = colors['olive'], size = (100,50), width = 0, backgroundcolor =colors["magenta"])
-    activemenu = mainmenu
+    
+    activemenu = menus["mainmenu"]
 
     while running:
         clock.tick(60)
@@ -135,9 +76,9 @@ def main():
                break 
            
 
-        if showmenu == 1:
+        if showmenu == True:
             screen.fill(activemenu.backgroundcolor)
-            showmenu = 0    
+            showmenu = False    
             for x in activemenu.menu_elements:
                 x.render()
 
@@ -149,12 +90,20 @@ def get_MousePos():
     pos = pygame.mouse.get_pos()
     return (pos)
 def checkhitmenu():
-    global activemenu_rects, activemenu
+    global activemenu_rects, activemenu, showmenu
     posx,posy = get_MousePos() 
     a=0
     for i in activemenu.menu_rects:
         if posx >= i.left and posx <= i.right and posy >= i.top and posy <= i.bottom :
             print(activemenu.menu_elements[a].msg)
+            try:
+                if menus[activemenu.menu_elements[a].msg]:
+                    activemenu = menus[activemenu.menu_elements[a].msg]
+                    showmenu = True
+                #elif activemenu.menu_elements[a].msg = "blabla": blabla()   menu olmayan tuşları bu şekilde kullanacaz
+
+            except :
+                pass
         a+=1
         pass
 
---
+++ menu.py
@@ -0,0 +1,63 @@
+import pygame
+class single:
+    def __init__(self,screen, pos, font, color, msg, background = False, backcolor = (0,0,0), size = (30,30), width = 0):
+        self.screen = screen
+        self.pos = pos
+        self.font = font
+        self.color = color
+        self.msg = msg
+        self.background = background
+        self.backcolor = backcolor
+        self.size = size
+        self.width = width
+
+        self.text = "Empty"
+        self.text_x = 0
+        self.text_y = 0
+        if self.background:
+            self.text = self.font.render(self.msg, True, self.color)
+            self.text_x = self.size[0]/2 - self.text.get_width()/2 + self.pos[0]
+            self.text_y = self.size[1]/2 - self.text.get_height()/2 + self.pos[1]
+        else:
+            self.text = self.font.render(self.msg, True, self.color)
+            self.text_x = self.size[0]/2 - self.text.get_width()/2 + self.pos[0]
+            self.text_y = self.size[1]/2 - self.text.get_height()/2 + self.pos[1]
+    def render(self):
+        if self.background:
+            pygame.draw.rect(self.screen,self.backcolor,(self.pos,self.size),self.width)
+            self.screen.blit(self.text,(self.text_x,self.text_y))
+        else:
+            self.screen.blit(self.text,(self.text_x,self.text_y))
+
+
+
+
+class menu:
+    def __init__(self, screen, pos, font ,color ,collumNline , gap, *msg, background = False, backcolor = (0,0,0), size = (30,30), width = 0, backgroundcolor = (255,0,0)):
+        self.menu_elements = []
+        self.menu_rects = []
+        self.gapx ,  self.gapy = gap
+        self.posx ,  self.posy = pos
+        self.sizex ,  self.sizey = size
+        self.collum ,  self.line= collumNline
+        self.a=0
+
+        self.screen = screen
+        self.pos = pos
+        self.font = font
+        self.color = color
+        self.msg = msg
+        self.background = background
+        self.backcolor = backcolor
+        self.size = size
+        self.width = width
+        self.backgroundcolor = backgroundcolor
+        for x in range( self.line):
+           for i in range( self.collum):
+               menu_element=single( self.screen, ( self.posx, self.posy),  self.font,  self.color,  self.msg[i+ self.a],  self.background,  self.backcolor,  self.size,  self.width)
+               self.menu_elements.append(menu_element)
+               self.menu_rects.append(pygame.Rect(( self.posx, self.posy), self.size))
+               self.posx =  self.posx +  self.gapx +  self.sizex
+           self.a+=2
+           self.posx =  self.pos[0]
+           self.posy =  self.posy +  self.sizey +  self.gapy
